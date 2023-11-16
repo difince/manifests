b24704ba8796b7431fec59548a6312bbc8d41d61
Diana Atanasova <dianaa@vmware.com>
2023-11-16 16:39:20 +0200

    Add script that does cve scanning with trivy based on the images files
    - add scan_images_for_sve script
    - add all images list per WG for branch v1.8-branch
    - add all sve scanning based on the images list for v1.8-branch
    
    Signed-off-by: Diana Atanasova <dianaa@vmware.com>

---
+++ hack/scan_images_for_cves.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v trivy)" ]; then
+  echo 'Error: trivy is not installed.' >&2
+  exit 1
+fi 
+
+IMAGE_LISTS_DIR="../docs/image_lists"
+
+# Check if the directory exists
+if [ ! -d "$IMAGE_LISTS_DIR" ]; then
+    echo "Directory not found: $IMAGE_LISTS_DIR"
+    exit 1
+fi
+
+CVE_REPORTS_DIR="../docs/cve-reports/"
+mkdir -p "$CVE_REPORTS_DIR"
+
+# Iterate through each file in the directory (excluding hidden files)
+for file in "$IMAGE_LISTS_DIR"/*; do
+    # Extract the file name without the path
+    filename=$(basename "$file")
+    cve_filename="$CVE_REPORTS_DIR/$filename"
+    echo "Scanning images from file: $file"
+      # Read the image names from the file and iterate through them
+      while IFS= read -r image_name; do
+          # Run your CVE scanning command for each image
+          # Replace the following line with your actual scanning command
+          echo "> Scanning image: $image_name"
+
+          trivy  image "$image_name" --scanners vuln -q >> "$cve_filename"
+
+          # Check the exit code of Trivy
+          if [ $? -ne 0 ]; then
+              echo "Error scanning $image_name" >> "$cve_filename"
+          fi
+      done < "$file"
+
+      echo "Done scanning images from file: $file"
+      echo "--||--||--||--||--||--||--||--||--||--||--||--"
+
+done
+
